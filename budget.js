1cba22795447c3a20f32dd50d22eb3380a157fa1
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-03-04 11:07:07 +0200

    Adding project to Github

---
+++ budget.js
@@ -0,0 +1,19 @@
+
+
+// SELECT ELEMENTS
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#outcome .list");
+const allList = document.querySelector("#all .list");
+
+// SELECT BUTTONS
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+// INPUT BUTTON
